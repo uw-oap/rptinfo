b317d51402f81db29ed9befabe1b120f06f9563f
jondavis-uw <jld36@uw.edu>
2025-06-03 15:28:28 -0700

    disable db on admin page

--- rpt-info/admin/class-rpt-info-admin.php
+++ rpt-info/admin/class-rpt-info-admin.php
@@ -59,7 +59,7 @@ class Rpt_Info_Admin {
 		$this->version = $version;
         $db_name = get_option('rpt_info_database_name');
         if ( $db_name) {
-            $this->rpt_db = new Rpt_Info_DB($db_name);
+//            $this->rpt_db = new Rpt_Info_DB($db_name);
         }
 	}
 
